104812d7e5062fc0c06eb8bccc986a7ea8bd7262
PC <PC@DESKTOP-M7S1RGQ>
2023-09-12 12:42:33 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Concrete.InMemory;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;//_productDal a ampulden generate constructor dediğimizde alttaki injection geliyor
+
+        public ProductManager(IProductDal productDal)//burası constructor injection yaptığımız yer
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş kodları burada yazılıyor
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+    }
+}
---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    //Çıplak class kalmasın standardı (bir class yanına interface ya da inheritance almıyorsa ileride problem yaşayacağız demektir.)
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
